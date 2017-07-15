926241d877a45f52cc411b1eeb206f34e6136d5b
Dan Jacob <dansamueljacob@gmail.com>
2017-07-14 23:00:03 -0700

    allow users to add products, remove belongs_to relationship with user and product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,57 @@
 class ProductsController < ApplicationController
+  before_action :only => [:new, :edit, :destroy]
 
 
   def index
-    @products = Product.all
+  @user = current_user
+  @products = Product.all
+end
+
+def new
+  @product = Product.new
+end
+
+def show
+  @product = Product.find(params[:id])
+end
+
+def destroy
+  @product = Product.find(params[:id])
+  @product.destroy
+  redirect_to products_path(@product)
+end
+
+def edit
+  @product = Product.find(params[:id])
+end
+
+def create
+  @product = Product.new(product_params)
+  if @product.save
+    flash[:notice] = "Product successfully added!"
+    redirect_to products_path
+  else
+    render :new
   end
+end
 
-  def new
-    @product = Product.new
+def update
+  @product = Product.find(params[:id])
+  if @product.update(product_params)
+    flash[:notice] = "Updated successfully"
+    redirect_to products_path
+  else
+    render :edit
   end
+end
 
-  def create
-    @product = Product.create(product_params)
+def edit
+@user = current_user
+@product = Product.find(params[:id])
+end
+
+private
+def product_params
+  params.require(:product).permit(:name, :description, :price)
   end
-
-  def show
-    @product = Product.find(params[:id])
-  end
-
-
-  private
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :image )
-  end
-
-
 end
